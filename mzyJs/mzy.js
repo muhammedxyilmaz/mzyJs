0b2c3748b81e9f220ee6540b2491f6d025a67b8f
unknown <muhammedxyilmaz@gmail.com>
2021-11-10 15:12:48 +0300

    mt
    mt

--- mzyJs/mzy.js
+++ mzyJs/mzy.js
@@ -1,4 +1,18 @@
-
+function fn_ObjectTypeFY(elementId) {
+    var d = typeof (elementId) === 'object' ? elementId : document.getElementById(elementId);
+    return d;
+}
+function id(elementId) {
+    return fn_ObjectTypeFY(elementId);
+}
+function fn_Hide(elementId) {
+    var d = fn_ObjectTypeFY(elementId);
+    if (d) { d.style.display = 'none'; }
+}
+function fn_Show(elementId) {
+    var d = fn_ObjectTypeFY(elementId);
+    if (d) { d.style.display = ''; }
+}
 
 
 
